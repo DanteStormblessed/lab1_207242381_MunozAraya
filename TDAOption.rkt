9d4ad7ec87c6a120b8a76c1cd969d52d891a5966
Alvaro <alvaro.munoz.a@usach.cl>
2023-09-20 12:27:02 -0300

    Se crean los archivos de funciones

---
+++ TDAOption.rkt
@@ -0,0 +1,9 @@
+#lang racket
+
+;La funcion "option" tiene como finalidad la construccion de opciones para el flujo de un Chatbot
+
+
+;----------------------CONSTRUCTOR----------------------
+;Dominio: code (Int)  X message (String)  X ChatbotCodeLink (Int) X FlowCodeLink (Int) X Keyword
+;Recorrido: Option
+
