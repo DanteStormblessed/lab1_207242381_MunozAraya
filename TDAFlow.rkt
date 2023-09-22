6c02a935f46c8739b36026277edd8b0cb0967837
Alvaro <alvaro.munoz.a@usach.cl>
2023-09-21 21:59:07 -0300

    Se crean las funciones CONSTRUCTOR de option y flow, MODIFICADOR de flow y sus respectivas descripciones

---
+++ TDAFlow.rkt
@@ -0,0 +1,24 @@
+#lang racket
+
+;__________________________________CONSTRUCTOR__________________________________
+
+;Dominio: id (int) X name (String)  X Option*
+;Recorrido: flow
+;Descripcion: Esta funcion construye el flujo a partir de una id 
+;Tipo de recursion: No aplica
+
+(define (flow id name . options)
+  (list id name options))
+
+;__________________________________MODIFICADOR__________________________________
+
+;Dominio: flow X option
+;Recorrido: flow
+;Descripcion: Esta funcion modifica el flow para poder añadir opciones
+;Tipo de recursion: No aplica
+
+(define (flow-add-option flow option)
+  (let ((options (cddr flow)))
+    (if (not (member option options)) 
+        (cons (car flow) (cadr flow) (cons option options)) 
+        flow)))
--- TDAOption.rkt
+++ TDAOption.rkt
@@ -3,7 +3,13 @@
 ;La funcion "option" tiene como finalidad la construccion de opciones para el flujo de un Chatbot
 
 
-;----------------------CONSTRUCTOR----------------------
+;__________________________________CONSTRUCTOR__________________________________
+
 ;Dominio: code (Int)  X message (String)  X ChatbotCodeLink (Int) X FlowCodeLink (Int) X Keyword
 ;Recorrido: Option
+;Descripcion: Crea una lista que contiene los elementos asociados al dominio de la misma funcion
+;Tipo de recursion: No aplica
 
+(define (option code message chatbot-code initial-flow-code . keywords)
+  (list code message chatbot-code initial-flow-code keywords))
+;_______________________________________________________________________________
