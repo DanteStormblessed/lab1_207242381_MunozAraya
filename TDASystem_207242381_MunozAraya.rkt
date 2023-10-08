037a5f88eaf07e8c563088ca6c7eee87f9f3c905
dante.stormblessed <alvaro.munoz.a@usach.cl>
2023-10-08 01:52:29 -0300

    Mas errores de duplicidad arreglados y mejora en System-talk-rec

--- TDASystem_207242381_MunozAraya.rkt
+++ TDASystem_207242381_MunozAraya.rkt
@@ -20,52 +20,81 @@
 )
 
 ;; TDA system - constructor
-(define (system name initialChatbotCodeLink)
-  (list name initialChatbotCodeLink '() '() '() #f)
-)
+(define (system name initialChatbotCodeLink . chatbots)
+  (define (add-chatbots chatbots chatbot-list chatbot-codes)
+    (if (null? chatbots)
+        (list chatbot-list chatbot-codes)
+        (let* ((chatbot (car chatbots))
+               (id (car chatbot)))
+          (if (member id chatbot-codes)
+              (add-chatbots (cdr chatbots) chatbot-list chatbot-codes)
+              (add-chatbots (cdr chatbots)
+                            (cons chatbot chatbot-list)
+                            (cons id chatbot-codes))))))
+  (let* ((chatbot-list-and-codes (add-chatbots chatbots '() '()))
+         (chatbot-list (car chatbot-list-and-codes))
+         (chatbot-codes (cadr chatbot-list-and-codes)))
+    (list name initialChatbotCodeLink chatbot-list chatbot-codes '() #f)))
+
 
 ;; TDA system - modificador para añadir chatbots a un sistema
 (define (system-add-chatbot system chatbot)
+  (define (chatbot-exists? chatbot-codes chatbotID)
+    (if (null? chatbot-codes)
+        #f
+        (if (equal? (car chatbot-codes) chatbotID)
+            #t
+            (chatbot-exists? (cdr chatbot-codes) chatbotID))))
   (let ((chatbot-list (third system))
         (chatbot-codes (fourth system)))
-    (if (not (member (cadr chatbot) chatbot-codes))
+    (if (chatbot-exists? chatbot-codes (car chatbot))
+        system
         (list (first system)
               (second system)
               (cons chatbot chatbot-list)
-              (cons (cadr chatbot) chatbot-codes)
+              (cons (car chatbot) chatbot-codes)
               (fifth system)
-              (sixth system))
-        system)
-    )
-)
+              (sixth system)))))
+
 
 ;; TDA system - modificador para añadir usuarios a un sistema
 (define (system-add-user system user)
+  (define (user-exists? users user)
+    (if (null? users)
+        #f
+        (if (equal? (car users) user)
+            #t
+            (user-exists? (cdr users) user))))
   (let ((users (fifth system)))
-    (if (not (member user users))
+    (if (user-exists? users user)
+        system
         (list (first system)
               (second system)
               (third system)
               (fourth system)
               (cons user users)
-              (sixth system))
-        system)
-    )
-)
+              (sixth system)))))
+
+
 
 ;; TDA system - función que permite iniciar una sesión en el sistema
 (define (system-login system user)
-  (let ((logged-user (sixth system)))
-    (if (not logged-user)
+  (define (user-exists? users user)
+    (if (null? users)
+        #f
+        (if (equal? (car users) user)
+            #t
+            (user-exists? (cdr users) user))))
+  (let ((users (fifth system))
+        (logged-user (sixth system)))
+    (if (or logged-user (not (user-exists? users user)))
+        system
         (list (first system)
               (second system)
               (third system)
               (fourth system)
               (fifth system)
-              user)
-        system)
-    )
-)
+              user))))
 
 ;; TDA system - función que permite cerrar una sesión abierta
 (define (system-logout system)
@@ -74,14 +103,14 @@
         (third system)
         (fourth system)
         (fifth system)
-        #f)
-)
+        #f))
+
 
 ;; system-talk-rec - Función que permite interactuar con un chatbot (usando recursividad)
 (define (system-talk-rec system message)
   (if (sixth system)
       (let ((logged-user (sixth system)))
-        (if (chatbot-exists? (fourth system) logged-user)
+        (if (find-chatbot-by-id (third system) logged-user)
             (let* ((chatHistory (seventh system))
                    (new-history (update-chatHistory chatHistory logged-user message)))
               (list (first system)
@@ -89,12 +118,12 @@
                     (third system)
                     (fourth system)
                     (fifth system)
-                    (sixth system)
+                    logged-user
                     new-history))
-            system)
-          system)
-      system)
-)
+            system))
+      system))
+
+
 
 ;; system-talk-norec - Función que permite interactuar con un chatbot (implementación declarativa)
 (define (system-talk-norec system message)
